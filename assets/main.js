86c8071d53eb6d7b1398ef914258fc4d6f3fcff2
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-10 20:32:24 +0100

    correzione dell'errore

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 const ul = document.querySelector('ul.list')
-for( let i = 0; i <= 100; i++ ){
+for( let i = 1; i <= 100; i++ ){
 
         if((i % 5 || i % 3) === 0){
             const element = `<li style="color: violet; border: 1px solid black; background-color: red;"> <span class="d-none">${i}</span> FizzBuzz </li>`
